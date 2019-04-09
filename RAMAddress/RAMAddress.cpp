ac89f49aa4da6e8afe2b7988d44e389e9f563f48
Gary Baugh <baughg@tcd.ie>
2019-04-09 19:55:50 +0100

    generic virtual to physical address mapping

--- RAMAddress/RAMAddress.cpp
+++ RAMAddress/RAMAddress.cpp
@@ -13,21 +13,36 @@ int main()
 	const uint32_t MASK_POS = 4;
 	const uint32_t CUT_ADDRESS_MASK = 0x1F << MASK_POS;
 	const uint32_t CUT_ADDRESS_MASK_4X = 0x7F << MASK_POS;
+    const uint32_t CUT_ADDRESS_MASK_10b = (1 << 11) - 1;
 	const uint32_t CUTS = 32;
 	
 	std::vector<uint32_t> histogram(CUTS);
 	std::vector<uint32_t> histogram_prop(CUTS);
 	uint32_t addr = 0;
 	uint32_t addr_7b = 0;
+    uint32_t addr_10b = 0;
 	uint32_t addr_offset = 0;
+    uint32_t addr_stagger = 0;
 
 	std::vector<uint32_t> cut_assignment(MAX_ADDRESS >> 4);
 	std::vector<uint32_t> cut_assignment_prop(MAX_ADDRESS >> 4);
 	std::ofstream cut_to_address_map("ram_cut_to_address_map.csv");
-	cut_to_address_map << "Address,RAM cut addr.,Start offset,RAM cut (VPU2), RAM cut (VPU3)" << std::endl;
+	cut_to_address_map << "Address,RAM cut addr.,Start offset,RAM cut (VPU2), RAM cut (VPU3), Stagger bits,";
+
+    for (uint32_t b = 0; b < 6; ++b)
+    {        
+        cut_to_address_map << "start_bits(" << b << ")";
+
+        if (b != 5)
+            cut_to_address_map << ",";
+    }
+    
+    cut_to_address_map << std::endl;
 
 	for (uint32_t a = 0; a < MAX_ADDRESS; a+=16)
 	{
+        addr_10b = (a >> MASK_POS) & CUT_ADDRESS_MASK_10b;
+        addr_stagger = addr_10b >> 5;
 		// old mapping
 		addr = (a & CUT_ADDRESS_MASK) >> MASK_POS;
 		histogram[addr]++;
@@ -41,14 +56,29 @@ int main()
 		histogram_prop[addr]++;
 		cut_assignment_prop[a >> 4] = addr;
 
-		cut_to_address_map		
-			<< a << ","
-			<< addr_7b << ","
-			<< addr_offset << ","
-			<< std::dec
-			<< cut_assignment[a >> 4] << ","
-			<< cut_assignment_prop[a >> 4]
-			<< std::endl;
+        cut_to_address_map
+            << a << ","
+            << addr_7b << ","
+            << addr_offset << ","
+            << std::dec
+            << cut_assignment[a >> 4] << ","
+            << cut_assignment_prop[a >> 4] << ","
+            << addr_stagger << ",";
+
+        for (uint32_t b = 0; b < 6; ++b)
+        {
+            uint32_t addr_b = (1 << b) - 1;
+            addr_b &= addr_stagger;
+            uint32_t shift_r = 9 - b;
+            uint32_t phy_addr = a & ~0x1ff;
+            phy_addr += (512  - (addr_b << shift_r) + (a & 0x1ff)) % 512;
+            cut_to_address_map  << phy_addr;
+
+            if (b != 5)
+                cut_to_address_map << ",";
+        }
+			
+        cut_to_address_map << std::endl;
     }
 
 	cut_to_address_map.close();
